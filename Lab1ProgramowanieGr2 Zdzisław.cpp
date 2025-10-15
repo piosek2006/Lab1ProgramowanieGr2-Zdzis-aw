e54cc5890f257395454f64c1bc81b7f94f600490
Piosek <piosek2006@gmail.com>
2025-10-15 13:13:46 +0200

    finale

--- Lab1ProgramowanieGr2 Zdzisław.cpp
+++ Lab1ProgramowanieGr2 Zdzisław.cpp
@@ -1,23 +1,63 @@
-﻿// Lab1ProgramowanieGr2 Zdzisław.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-	cout << "Hello World!\n";
+	// Zadanie 1
+	
+	/*string imie, nazwisko;
+	cout << "Podaj imie: ";
+	cin >> imie;
+	cout << "Podaj nazwisko: ";
+	cin >> nazwisko;
+	cout << "Twoje inicjaly to: " << imie[0] << "." << nazwisko[0] << "." << endl;
+	*/
+	
+
+	// Zadanie 2
+	/*cout << "Gwiazda 5-ramienna" << endl;
+		cout << "   *  " << endl;
+		cout << "  * * " << endl;
+		cout << "**   **" << endl;
+		cout << " * * *" << endl;
+		cout << "*     *" << endl;*/
+
+	// Zadanie 3
+	/*cout << "Lista Obecnosci" << endl;
+	cout << "nr\t|\timie\t\t|\tnazwisko\n";
+	cout << "------------------------------------------------------\n";
+	cout << "1\t|\tZdzislaw\t|\tKowalski\n";
+	cout << "2\t|\tZygfryd\t\t|\tBolonski\n";
+	cout << "3\t|\tLeonidas\t|\tFranciszek\n";*/
+
+	// Zadanie 4
+	/*float a, b;
+	cout << "Podaj dwie liczby: \n";
+	cin >> a >> b;
+	cout << "Suma: " << a + b << endl;
+	cout << "Iloczyn: " << a * b << endl;*/
+
+	// Zadanie 5
+	float f, c;
+	cout << "Podaj temperature w stopniach Fahrenheita: ";
+	cin >> f;
+	c = (f - 32) * 5 / 9;
+	cout << f << " stopni Fahrenheita to " << c << " stopni Celsjusza";
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// 2137 = (x - 32) * 5 / 9
+// 2137 = 0.6x - 17.78
+// 2137 + 17.78 = 0.6x
+// 2154.78 = 0.6x
+// 2154.78 / 0.6 = x
+// x = 3591.3
+// 2137 + 0.64 = 0.6x
+// 2137.64 = 0.6x
+// 2137.64 / 0.6 = x
+// x = 3562.7333
+// 2137 * 9 / 5 + 32 = x
+// x = 3853.6 + 32
+// x = 3885.6
+// x = 3878.6 FINAL ANSWER
